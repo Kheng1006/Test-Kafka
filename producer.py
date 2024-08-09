12627433d402f3df076e52f3a61203be330c0083
Le Duy Khang <khangnbk7415@gmail.com>
2024-08-09 15:00:39 +0700

    check

--- producer.py
+++ producer.py
@@ -1,11 +1,27 @@
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import csv
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'client.id': 'csv-producer'
+}
 
-with open('./test.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        message = ','.join(row)
-        producer.send('csv-data', value=message.encode('utf-8'))
+producer = Producer(conf)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+
+csv_file_path = 'test.csv'
+topic = 'test-topic'
+
+with open(csv_file_path, mode='r') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        message = ','.join(row)  
+        producer.produce(topic, value=message, callback=delivery_report)
+        producer.poll(0)  
+
 producer.flush()
