815880cd0dfa14ab49dcf824278442a2d01e6f1a
skysky229 <huy.nguyen229@hcmut.edu.vn>
2024-08-11 17:32:24 +0700

    integrate hadoop and spark

---
+++ scripts/read_from_hadoop.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("ReadParquetFromHDFS") \
+    .getOrCreate()
+
+# Define the HDFS path to the Parquet file
+parquet_path = "hdfs://test-kafka-namenode-1:9000/output_test"
+
+# Read the Parquet file
+df = spark.read.parquet(parquet_path)
+# Show the DataFrame content
+df.show()
+
+# Print the schema of the DataFrame
+df.printSchema()
